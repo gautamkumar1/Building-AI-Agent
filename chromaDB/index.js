3d11876c6cd0c5de60fa7c462d5548c50b74044e
gautamkumar1 <gautamraaz936@gmail.com>
2025-02-26 13:49:26 +0530

    Emebeddings created

--- chromaDB/index.js
+++ chromaDB/index.js
@@ -1,4 +1,4 @@
-import { ChromaClient } from "chromadb";
+import { ChromaClient,OpenAIEmbeddingFunction } from "chromadb";
 
 const client = new ChromaClient({
     path: "http://localhost:8000"
@@ -7,9 +7,9 @@ const client = new ChromaClient({
 const main = async () => {
     try {
         const collection = await client.createCollection({
-            name: "my_collection",
+            name: "my_collection2",
             metadata: {
-                "description": "My first collection"
+                "description": "My first collection2"
             }
         });
         console.log("Collection created:", collection);
@@ -17,16 +17,23 @@ const main = async () => {
         console.error("Error creating collection:", error);
     }
 };
-
+const embeddingFunction = new OpenAIEmbeddingFunction({
+    openai_api_key: process.env.OPENAI_API_KEY, // Ensure this is set
+    openai_model: "text-embedding-3-small"
+});
 const addData = async () => {
     try {
-        const collection = await client.getCollection({ name: "my_collection" });
+        const collection = await client.getCollection(
+            { name: "my_collection2",
+            embeddingFunction: embeddingFunction,
+            },
+            
+        );
         const response = await collection.add({
             ids: ["id1", "id2"],
            // Embeddings are mandatory that genrated by openai 
-            embeddings: [[1, 2, 3], [4, 5, 6]],
-            metadatas: [{ "key": "value" }, { "key": "value" }],
-            documents: ["document1", "document2"]
+            // embeddings: [[1, 2, 3], [4, 5, 6]],
+            documents: ["This is my entry 1", "This is my entry 2"]
         });
         console.log("Data added successfully!");
         const results = await collection.get({ ids: ["id1", "id2"] });
