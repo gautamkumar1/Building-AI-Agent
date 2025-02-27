d7390bdabc973f6dbd121f997c496ceadec44988
gautamkumar1 <gautamraaz936@gmail.com>
2025-02-27 15:48:47 +0530

    Learn about pinecone db

--- pineconeDB/index.js
+++ pineconeDB/index.js
@@ -5,8 +5,8 @@ const pc = new Pinecone({
 });
 
 const indexName = 'learning-pincone-db';
-
-const main = async () =>{
+// creating a index
+const createIndex = async () =>{
     await pc.createIndex({
         name: indexName,
         dimension: 1536, 
@@ -19,5 +19,79 @@ const main = async () =>{
         } 
       });
 }
+// Namespace means divide indexs in subgroups 
+const createNameSpace = async () =>{
+    const index = await getIndex();
+    const namespace = index.namespace("learning-namespace");
+    return namespace;
+}
+// Generate random vectors
+const generateRandomVectors = (length) => {
+  return Array.from({ length }, () => Math.random());
+};
+const upsertVectors = async () =>{
+    try {
+      const index = await getIndex();
+      // console.log("index",index);
+      
+    const embedding = generateRandomVectors(1536);
+    const res = await index.upsert([
+      {
+        "id": "2",   // Unique ID
+        "values": embedding, // Embedding Vector
+        "metadata": {
+          "name": "test2",
+          "age": 20,
+          "gender": "male"
+        }
+      }
+    ]);
+    console.log("Upsert record successfully");
+    // console.log(res," ++++++++++++++++++++++++++");
+    } catch (error) {
+      console.log("error",error);
+      
+    }
+    
+}
 
-// main();
+const queryVectors = async () =>{
+    const index = await getIndex();
+    const query = await index.query({
+        "id": "2",
+        topK: 1,
+        includeMetadata: true,
+      });
+    console.log(query);
+}
+const listIndex = async () =>{
+    const indexList = await pc.listIndexes();
+    console.log(indexList);
+}
+
+const getIndex = async () =>{
+    const index = pc.index("learning-pincone-db");
+    return index;
+}
+const describeIndex = async () =>{
+    const decribeData = await pc.describeIndex('learning-pincone-db');
+    console.log(decribeData);
+}
+const createCollection = async () =>{
+    const res = await pc.createCollection({
+        name: "learning-collection",
+        source: "learning-pincone-db",
+      });
+    console.log(res);
+}
+const main = async () =>{
+    // await createIndex();
+    // await listIndex();
+    // await getIndex();
+    // await describeIndex();
+    // await createCollection();
+    // await upsertVectors();
+    await queryVectors();
+}
+
+main();
